afcf11fca12623a9c3dfce32b948333b15e6d949
mucahitkarakus <mocococo.80@gmail.com>
2022-11-17 13:47:24 +0300

    register metarial ui done

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Dashboard from "../Pages/Dashboard";
 import Login from "../Pages/Login";
+import Register from "../Pages/Register";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/Login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
