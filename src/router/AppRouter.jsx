5a90813d63b21ee31e36889a55a406fcf05d36e4
mucahitkarakus <mocococo.80@gmail.com>
2022-11-17 13:42:17 +0300

    metarial ui login page done

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Dashboard from "../Pages/Dashboard";
+import Login from "../Pages/Login";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/Login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
