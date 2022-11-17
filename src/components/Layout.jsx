f38e89a1d4f7d906b7157d84789eb9f26d4c5c15
mucahitkarakus <mocococo.80@gmail.com>
2022-11-17 13:23:48 +0300

    layout dashboarda bağlandı

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Navbar from "./Navbar";
-const Layout = () => {
+const Layout = ({children}) => {
   return (
     <div>
       <Navbar />
+      {children}
     </div>
   );
 };
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,10 @@
-import React from "react";
+import React, {Children} from "react";
 import Layout from "../components/Layout";
 
-const Dashboard = () => {
-  const data = {};
-
+const Dashboard = ({}) => {
   return (
     <Layout>
-      <h1>DashBoard</h1>
+      <h1 className="text-center text-2xl ">DASHBOARD</h1>
     </Layout>
   );
 };
