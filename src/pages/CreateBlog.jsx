842d79c0e7fa846ed91d27334ac60c8847e97fbc
mucahitkarakus <mocococo.80@gmail.com>
2022-11-19 11:13:57 +0300

    createblog style done

---
+++ src/pages/CreateBlog.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import BlogCard from "../components/BlogCard";
+import Layout from "../components/Layout";
+
+const CreateBlog = () => {
+  return (
+    <Layout>
+      <div className="max-w-lg sm:mx-auto bg-slate-200 mt-10 mx-5">
+        <BlogForm title_="Create New Post" />
+      </div>
+    </Layout>
+  );
+};
+
+export default CreateBlog;
