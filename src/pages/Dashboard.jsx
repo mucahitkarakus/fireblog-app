010514e14e784e62e8558c79c6fd2f2365e03c02
mucahitkarakus <mocococo.80@gmail.com>
2022-11-19 11:12:37 +0300

    dashboard style done

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,14 @@
-import React, {Children} from "react";
+import React from "react";
+import BlogCard from "../components/BlogCard";
 import Layout from "../components/Layout";
 
 const Dashboard = () => {
   return (
     <Layout>
-      <h1 className="text-center text-2xl ">DASHBOARD</h1>
+      <p className="text-center text-3xl font-extrabold mt-10">Dashboard</p>
+      <div className="bg-gray-100 rounded mt-10 max-w-screen-xl mx-auto">
+        <div className="p-5 flex flex-wrap gap-10 justify-center items-center"></div>
+      </div>
     </Layout>
   );
 };
